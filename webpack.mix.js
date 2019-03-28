cb859f41f657e5aca12d53b21002691395a09b10
abraham ghaemi <ig.ghaemi@gmail.com>
2019-03-28 23:15:38 +0430

    init

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+const mix = require('laravel-mix');
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync({
+	proxy: 'homestead.test'
+});
