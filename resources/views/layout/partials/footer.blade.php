35b219a5c55c34e873ad80c5d9d90b7ab6280061
abraham ghaemi <snapycloud@gmail.com>
2019-04-05 03:49:46 +0430

    update footer

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -5,7 +5,7 @@
 						<div class="column width-12 center">
 							<div class="widget">
 								<h3 class="mb-30">Snapy Cloud Very Fastly</h3>
-								<p class="lead mb-30">ارائه دهنده پلتفرم سرویس api و نرم افزار مدیریت ارتباط با شمتیر<br>اسنپی کلود</p>
+								<p class="lead mb-30 rtl">ارائه دهنده پلتفرم سرویس api و نرم افزار مدیریت ارتباط با مشتری<br>اسنپی کلود</p>
 {{-- 								<ul class="social-list list-horizontal center">
 									<li><a href="#"><span class="icon-twitter small"></span></a></li>
 									<li><a href="#"><span class="icon-facebook small"></span></a></li>
